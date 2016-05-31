39bf443e5d00bf572efe092351d5d2f8b7ace206
Antoine <aghigny@gmail.com>
2016-05-31 22:40:55 +0200

    implementing the routes for the api

---
+++ server/bin/api/signup.js
@@ -0,0 +1,23 @@
+"use strict";
+function signup(apiRoutes, User) {
+    apiRoutes.post('/signup', function (req, res) {
+        if (!req.body.name || !req.body.password) {
+            res.json({ success: false, msg: 'Please pass name and password.' });
+        }
+        else {
+            var newUser = new User({
+                name: req.body.name,
+                password: req.body.password
+            });
+            newUser.save(function (err) {
+                if (err) {
+                    return res.json({ success: false, msg: 'Username already exists.' });
+                }
+                res.json({ success: true, msg: 'Successful created new user.' });
+            });
+        }
+    });
+}
+exports.signup = signup;
+;
+//# sourceMappingURL=signup.js.map
