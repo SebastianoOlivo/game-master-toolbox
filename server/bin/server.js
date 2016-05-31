2741751b30d177640fba1a01fa6960788fde8158
Antoine <aghigny@gmail.com>
2016-05-31 22:41:38 +0200

    Create the base server

--- server/bin/server.js
+++ server/bin/server.js
@@ -1,13 +1,23 @@
 "use strict";
-/// <reference path="../typings/index.d.ts" />
 var express = require('express');
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var database_1 = require('./config/database');
+var user_1 = require('./models/user');
+var signup_1 = require('./api/signup');
 var port = process.env.PORT || 8080;
 var app = express();
+var apiRoutes = express.Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
+mongoose.connect(database_1.Config.database);
 app.get('/', function (req, res) {
-    res.send('Hello dragons !!');
+    res.send('Hello fellows !! ;-)');
 });
+signup_1.signup(apiRoutes, user_1.User);
+app.use('/api', apiRoutes);
 app.listen(port);
 console.log('started server on port ' + port);
 //# sourceMappingURL=server.js.map
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,36 @@
 /// <reference path="../typings/index.d.ts" />
+
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
 import * as passport from 'passport';
-import * as jwt from 'jwt-simple'; 
-import {database} from './config/database';
+import * as jwt from 'jwt-simple';
+
+import { Config } from './config/database';
+import { User } from './models/user';
+import { signup } from './api/signup';
 
 let port = process.env.PORT || 8080;
 let app = express();
+let apiRoutes = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res) {
-   res.send('Hello dragons !!');
+mongoose.connect(Config.database);
+
+//Create the routes
+
+app.get('/', (req, res) => {
+    res.send('Hello fellows !! ;-)');
 });
 
-app.listen(port);
+signup(apiRoutes, User);
 
+app.use('/api', apiRoutes);
+
+app.listen(port);
 console.log('started server on port ' + port);
